d8a4d9da6e7297cb4d5089fbb0cf7bc2a5242cee
André <60563950+andrexxrojas@users.noreply.github.com>
2024-03-24 16:29:11 +0800

    Added functionality to table view page
    Can now add group tasks, as well as as tasks to the new tables created

--- tableScript.js
+++ tableScript.js
@@ -1,4 +1,5 @@
 let groups = document.querySelectorAll(".group")
+let tableContainer = document.querySelector(".tableExtraContainer")
 
 //For adding new group
 let groupContainer = document.querySelector(".groupTask")
@@ -34,6 +35,8 @@ addGroupBtn.addEventListener("click", function(){
         groupContainer.appendChild(group)
 
         updateGroupsArray();
+        createTable(newGroupTitle);
+
     }
 
     console.log(groups);
@@ -41,5 +44,110 @@ addGroupBtn.addEventListener("click", function(){
 
 function updateGroupsArray(){
     groups = document.querySelectorAll(".group")
+}
+
+function createTable(tableTitle){
+    let table = document.createElement("div")
+    table.classList = "table"
+
+    let titleContainer = document.createElement("div")
+    titleContainer.classList = "titleContainer"
+    let title = document.createElement("p")
+    title.innerHTML = tableTitle
+
+    titleContainer.appendChild(title)
+    table.appendChild(titleContainer)
+
+    let contentTable = document.createElement("table")
+    contentTable.classList = "contentTable"
+
+    let tbody = document.createElement("tbody")
+    contentTable.appendChild(tbody)
+
+    table.appendChild(contentTable)
+
+    let createNewTask = document.createElement("div")
+    createNewTask.classList = "createNewTask"
+
+    let icon = document.createElement("i")
+    icon.classList = "fa-solid fa-plus"
+
+    let createTaskP = document.createElement("p")
+    createTaskP.innerHTML = "Create task"
+    
+    let newBox = document.createElement("div")
+    newBox.classList = "newBox"
+
+    //For adding new rows
+    createNewTask.addEventListener("click", function(){
+        if(createNewTask.classList != "active"){
+            newBox.style.display = "block";
+            createNewTask.classList.add("active")
+        }
+    })
+
+    newBox.addEventListener("keyup", function(event){
+        let textInput = newBox.querySelector("input").value;
+
+        if(event.keyCode == 13){
+            if(textInput == ""){
+                newBox.style.display = "none"
+                createNewTask.classList.remove("active")
+            }else{
+                //Add new row function
+                let row = document.createElement("tr")
+
+                let taskTitle = document.createElement("td")
+                taskTitle.classList = "taskTitle"
+                taskTitle.innerHTML = textInput
+
+                let taskProgress = document.createElement("td")
+                taskProgress.classList = "taskProgress"
+
+                let customSelect = document.createElement("div")
+                customSelect.classList = "custom-select"
+
+                let select = document.createElement("select")
+                let optionOne = document.createElement("option")
+                optionOne.value = "todo"
+                optionOne.innerHTML = "TO DO"
+                let optionTwo = document.createElement("option")
+                optionTwo.value = "inprogress"
+                optionTwo.innerHTML = "IN PROGRESS"
+                let optionThree = document.createElement("option")
+                optionThree.value = "done"
+                optionThree.innerHTML = "DONE"
+
+                select.appendChild(optionOne)
+                select.appendChild(optionTwo)
+                select.appendChild(optionThree)
+
+                customSelect.appendChild(select)
+                taskProgress.appendChild(customSelect)
+
+                row.appendChild(taskTitle)
+                row.appendChild(taskProgress)
+
+                tbody.appendChild(row)
+                //Add new row function
+            }
+
+        }
+    })
+    //For adding new rows
+
+    let input = document.createElement("input")
+    input.setAttribute("type", "text")
+    input.setAttribute("placeholder", "What is the task you want to add?")
+
+    newBox.appendChild(input)
+
+    createNewTask.appendChild(icon)
+    createNewTask.appendChild(createTaskP)
+    createNewTask.appendChild(newBox)
+
+    table.appendChild(createNewTask)
+    
+    tableContainer.appendChild(table)
 }
 
